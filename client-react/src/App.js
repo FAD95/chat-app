7b03ea1733a2692186b3be9e687b05f80a8d35bc
Fadith Escorcia <feescorciam@protonmail.com>
2020-09-08 00:19:50 -0500

    [ADD] Bootsrap and global styles

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import GlobalStyles from './styles/GlobalStyles'
 
 import Join from './components/Join'
 import Chat from './components/Chat'
 
 const App = () => (
-  <Router>
-    <Route path='/' component={Join}></Route>
-    <Route path='/chat' component={Chat}></Route>
-  </Router>
+  <>
+    <GlobalStyles />
+    <Router>
+      <Route path='/' exact component={Join}></Route>
+      <Route path='/chat' component={Chat}></Route>
+    </Router>
+  </>
 )
 export default App
