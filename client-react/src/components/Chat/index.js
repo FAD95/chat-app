952cd17677647f6fdeae37e8e4302f3ccd4ac9c5
Fadith Escorcia <feescorciam@protonmail.com>
2020-09-08 04:12:53 -0500

    [ADD] Listado y envio de mensajes

--- client-react/src/components/Chat/index.js
+++ client-react/src/components/Chat/index.js
@@ -7,15 +7,19 @@ let socket
 const Chat = ({ location }) => {
   const [nameSession, setNameSession] = useState('')
   const [roomSession, setRoomSession] = useState('')
+
+  const [message, setMessage] = useState([])
+  const [messages, setMessages] = useState([])
+
   const ENDPOINT = 'localhost:5000'
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search)
+
     socket = io(ENDPOINT)
 
     setNameSession(name)
     setRoomSession(room)
-    console.log({ nameSession, roomSession })
 
     socket.emit('join', { name, room }, () => {})
 
@@ -24,7 +28,33 @@ const Chat = ({ location }) => {
       socket.off()
     }
   }, [ENDPOINT, location.search])
-  return <h1>Chat</h1>
+
+  useEffect(() => {
+    socket.on('message', (message) => {
+      setMessages([...messages, message])
+    })
+  }, [messages])
+
+  const sendMessage = (e) => {
+    e.preventDefault()
+
+    if (message) {
+      socket.emit('sendMessage', message, () => setMessage(''))
+    }
+  }
+  console.log(messages)
+  return (
+    <div>
+      <div>
+        <input
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyPress={(e) => (e.key === 'Enter' ? sendMessage(e) : null)}
+          type='text'
+        />
+      </div>
+    </div>
+  )
 }
 
 export default Chat
