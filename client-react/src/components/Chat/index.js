35866e1a5a2d4a9a7603272290986cf2e40aefad
Fadith Escorcia <feescorciam@protonmail.com>
2020-09-08 01:09:23 -0500

    [ADD] Emitiendo el nombre y la sala

--- client-react/src/components/Chat/index.js
+++ client-react/src/components/Chat/index.js
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import queryString from 'query-string'
+import io from 'socket.io-client'
 
-const Chat = () => {
+let socket
+
+const Chat = ({ location }) => {
+  const [nameSession, setNameSession] = useState('')
+  const [roomSession, setRoomSession] = useState('')
+  const ENDPOINT = 'localhost:5000'
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search)
+    socket = io(ENDPOINT)
+
+    setNameSession(name)
+    setRoomSession(room)
+    console.log({ nameSession, roomSession })
+
+    socket.emit('join', { name, room }, () => {})
+
+    return () => {
+      socket.emit('disconnect')
+      socket.off()
+    }
+  }, [ENDPOINT, location.search])
   return <h1>Chat</h1>
 }
 
