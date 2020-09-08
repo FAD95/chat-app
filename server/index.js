1dd2ccff8eb5f3f33d8ab9c089a2b3043a190bef
Fadith Escorcia <feescorciam@protonmail.com>
2020-09-07 22:20:21 -0500

    Configuracion inicial

--- server/index.js
+++ server/index.js
@@ -1,1 +1,17 @@
-console.log('Holi guapo')
+const express = require('express')
+const socketio = require('socket.io')
+const http = require('http')
+
+const PORT = process.env.PORT || 5000
+
+const router = require('./src/router')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+app.use(router)
+
+server.listen(PORT, () =>
+  console.log(`Server is running on http://localhost:${PORT}`)
+)
