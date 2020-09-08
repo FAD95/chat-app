135ff9d47e3ed43fd2f3ed730bfcc74abd570785
Fadith Escorcia <feescorciam@protonmail.com>
2020-09-08 04:11:20 -0500

    [ADD] Mensaje al iniciar y sendMessage

--- server/index.js
+++ server/index.js
@@ -17,11 +17,30 @@ io.on('connection', (socket) => {
 
   socket.on('join', ({ name, room }, cb) => {
     const { error, user } = addUser({ id: socket.id, name, room })
-
     if (error) cb(error)
 
+    socket.emit('message', {
+      user: 'admin',
+      text: `Hello ${user.name}, welcome to ${user.room}`,
+    })
+    socket.broadcast.to(user.room).emit('message', {
+      user: 'admin',
+      text: `${user.name} has joined!`,
+    })
+
     socket.join(user.room)
+
+    cb()
   })
+
+  socket.on('sendMessage', (message, cb) => {
+    const user = getUser(socket.id)
+
+    io.to(user.room).emit('message', { user: user.name, text: message })
+
+    cb()
+  })
+
   socket.on('disconnect', () => {
     console.log('User had left')
   })
