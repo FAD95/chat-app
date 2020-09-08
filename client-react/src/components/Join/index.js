61ff95e65798c823f84409aa1f515cef01cbe01e
Fadith Escorcia <feescorciam@protonmail.com>
2020-09-08 00:19:18 -0500

    [ADD] Join styles and state for the inputs to share it with Chat

--- client-react/src/components/Join/index.js
+++ client-react/src/components/Join/index.js
@@ -1,7 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { Container, JoinForm, FormName } from './styles'
 
 const Join = () => {
-  return <h1>Join</h1>
+  const [name, setName] = useState('')
+  const [room, setRoom] = useState('')
+
+  const handleSubmitError = (e) => {
+    e.preventDefault()
+    if (!name) {
+      alert('You have to enter a name')
+    }
+    if (!room) {
+      alert('You have to enter the room')
+    }
+  }
+
+  return (
+    <Container>
+      <JoinForm>
+        <FormName>Join</FormName>
+        <div className='form-group mt-5'>
+          <input
+            type='text'
+            placeholder='Name'
+            onChange={(e) => setName(e.target.value)}
+            className='form-control'
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Room'
+            onChange={(e) => setRoom(e.target.value)}
+            className='form-control'
+          />
+        </div>
+        <Link
+          onClick={(e) => (!name || !room ? handleSubmitError(e) : null)}
+          to={`/chat?name=${name}&room=${room}`}
+        >
+          <button className='btn btn-primary mt-3' type='submit'>
+            Sign In
+          </button>
+        </Link>
+      </JoinForm>
+    </Container>
+  )
 }
 
 export default Join
---
+++ client-react/src/components/Join/styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
+
+export const JoinForm = styled.form`
+  background-color: #111;
+  opacity: 0.7;
+  box-sizing: border-box;
+  width: 400px;
+  max-width: 80vw;
+  min-width: 300px;
+  padding: 50px 50px;
+  height: 400px;
+  border-radius: 10px;
+  text-align: center;
+`
+export const FormName = styled.h1`
+  color: #fff;
+  font-family: 'Courier New', Courier, monospace;
+`
