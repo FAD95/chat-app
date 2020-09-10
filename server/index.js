c2f633de1bf9206f06f48a51d6e2417771eb206f
Fadith Escorcia <feescorciam@protonmail.com>
2020-09-10 02:23:47 -0500

    cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const socketio = require('socket.io')
 const http = require('http')
+const cors = require('cors')
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./src/users')
 
@@ -74,7 +75,7 @@ io.on('connection', (socket) => {
 })
 
 app.use(router)
-
+app.use(cors())
 server.listen(PORT, () =>
   console.log(`Server is running on http://localhost:${PORT}`)
 )
