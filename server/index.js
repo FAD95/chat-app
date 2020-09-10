aff7afb4afb1aabc93f051d9b4dbb3f004b71ed8
Fadith Escorcia <feescorciam@protonmail.com>
2020-09-10 02:13:47 -0500

    working

--- server/index.js
+++ server/index.js
@@ -19,30 +19,57 @@ io.on('connection', (socket) => {
     const { error, user } = addUser({ id: socket.id, name, room })
     if (error) cb(error)
 
+    const date = new Date()
+    let hour = date.getHours()
+    let minutes =
+      date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()
+    const time = hour + ':' + minutes
+
     socket.emit('message', {
       user: 'admin',
       text: `Hello ${user.name}, welcome to ${user.room}`,
+      date: time,
     })
     socket.broadcast.to(user.room).emit('message', {
       user: 'admin',
       text: `${user.name} has joined!`,
+      date: time,
     })
 
     socket.join(user.room)
 
+    io.to(user.room).emit('roomData', {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    })
     cb()
   })
 
   socket.on('sendMessage', (message, cb) => {
     const user = getUser(socket.id)
 
-    io.to(user.room).emit('message', { user: user.name, text: message })
+    io.to(user.room).emit('message', {
+      user: user.name,
+      text: message.text,
+      date: message.date,
+    })
+    io.to(user.room).emit('roomData', {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    })
 
     cb()
   })
 
   socket.on('disconnect', () => {
-    console.log('User had left')
+    const user = removeUser(socket.id)
+
+    if (user) {
+      io.to(user.room).emit('message', {
+        user: 'admin',
+        text: `${user.name} has left.`,
+      })
+    }
   })
 })
 
